d543e485698c65e72d5f5e13b68431a1f4daee65
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2024-10-08 13:19:06 +0530

    Add files via upload

---
+++ backend/dist/config/openai-config.js
@@ -0,0 +1,15 @@
+/*import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export const configureGemini = () => {
+  const apiKey = process.env.GEMINI_API_KEY; // Make sure to set your API key in your environment variables
+  return new GoogleGenerativeAI(apiKey);
+};*/
+import { Configuration } from "openai";
+export const configureOpenAI = () => {
+    const config = new Configuration({
+        apiKey: process.env.OPEN_AI_SECRET,
+        organization: process.env.OPENAI_ORAGANIZATION_ID,
+    });
+    return config;
+};
+//# sourceMappingURL=openai-config.js.map
